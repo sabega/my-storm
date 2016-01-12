13de6e3c103c83464a8d12d9849ed89ef0e7994e
Samuel Benavides <samuel.benavides.garcia@gmail.com>
2016-01-12 09:45:51 +0100

    Initial commit

---
+++ src/main/java/org/uma/samuel/asignatura/App.java
@@ -0,0 +1,13 @@
+package org.uma.samuel.asignatura;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
